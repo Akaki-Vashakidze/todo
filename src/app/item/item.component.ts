085a6c0bec815f26e25993068f5f34b5497f64d1
Akaki <Akaki@Pro1.local>
2022-09-22 14:05:44 +0400

    first

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from "@angular/core";
+import { Item } from "../item.model";
+import { ItemsService } from "../items.service";
+
+@Component({
+    selector:"app-item",
+    templateUrl:"./item.component.html",
+    styleUrls:['./item.component.css']
+})
+
+export class ItemComponent {
+  @Input() item:Item;
+  @Input() itemIndex:number;
+
+  constructor(private itemsService:ItemsService) {}
+
+  onDeleteItem(){
+    this.itemsService.deleteItem(this.itemIndex)
+  }
+
+  finishTask(){
+   this.itemsService.finishedTask(this.itemIndex)
+  }
+}
+
---
+++ src/app/items.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core"
+import { Item } from "./item.model"
+
+@Injectable({providedIn:'root'})
+export class ItemsService {
+  items : Item[] = [
+    {description:'davrekot dgf',done:false},
+    {description:'gavarvizot', done:true},
+    {description:'vchamo', done:false}
+  ]
+
+  setItems(){
+    localStorage.setItem('todos',JSON.stringify(this.items))
+  }
+
+  getItems(){
+   let savedItems = localStorage.getItem('todos');
+   if (savedItems) {
+    this.items = JSON.parse(savedItems);
+   }
+   return this.items;
+  }
+
+  addItems(newItemDesc:string) {
+    this.items.unshift({description:newItemDesc,done:false})
+    this.setItems()
+  }
+
+  deleteItem(index:number) {
+   this.items.splice(index,1)
+   this.setItems()
+  }
+
+  finishedTask(index2:number) {
+    this.items[index2].done = !this.items[index2].done ;
+    this.setItems()
+  }
+
+}
